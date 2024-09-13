bafe2bf9130c5b0aba82670075e8238891938a48
Pedro Quintas <pedroquintas1213@gmail.com>
2024-09-13 10:26:43 -0300

    alteração na exibição de autores/reus no corpo do email

--- src/main/java/com/envio/email/controllers/IndexController.java
+++ src/main/java/com/envio/email/controllers/IndexController.java
@@ -2,9 +2,7 @@ package com.envio.email.controllers;
 
 import com.envio.email.dtos.ProcessoDTO;
 
-import com.envio.email.models.Cliente;
-import com.envio.email.models.Processo;
-import com.envio.email.models.Processo_docinicial;
+import com.envio.email.models.*;
 import com.envio.email.services.AutorReuService;
 import com.envio.email.services.ClienteService;
 import com.envio.email.services.CompaniesSevice;
@@ -25,7 +23,8 @@ public class IndexController {
     private final CompaniesSevice companiesSevice;
     private final DocInicialService docInicialService;
 
-    public IndexController(AutorReuService autorReuService, ClienteService clienteService, CompaniesSevice companiesSevice, DocInicialService docInicialService) {
+    public IndexController(AutorReuService autorReuService, ClienteService clienteService,
+                           CompaniesSevice companiesSevice, DocInicialService docInicialService) {
         this.autorReuService = autorReuService;
         this.clienteService = clienteService;
         this.companiesSevice = companiesSevice;
@@ -35,22 +34,31 @@ public class IndexController {
 
     @GetMapping("/")
     public String DadosProcesso(Model model, @RequestParam(required = false) String status, @RequestParam(required = false) Integer codEscritorio) {
-        List<ProcessoDTO> autorReuDTOList = autorReuService.buscarAutoresReusPorParametros("P", 1303);
+        List<ProcessoDTO> autorReuDTOList = autorReuService.buscarAutoresReusPorParametros("P", 1165);
+        var limiteLinks = 5;
+        var limitepessoas = 1;
 
         if (!autorReuDTOList.isEmpty()) {
             Integer codEscritorioDoDTO = autorReuDTOList.get(0).getCodEscritorio();
             List<Cliente>clientes = clienteService.BuscarPorEscritorio(codEscritorioDoDTO);
             model.addAttribute("cliente", clientes);
         }
+
         //Limita os links exibidos em tela
         List<Processo_docinicial> links = autorReuDTOList.get(0).getLink();
-        var limiteLinks = 5;
 
         if (links.size()> limiteLinks){
                 links = links.subList(0, limiteLinks);
         }
-
+        List<Processo_reu> Reu = autorReuDTOList.get(0).getReus();
+        List<Processo_autor> Autor = autorReuDTOList.get(0).getAutores();
+        if (Reu.size() > limitepessoas || Autor.size()>limitepessoas ) {
+            Reu = Reu.subList(0, limitepessoas);
+            Autor = Autor.subList(0, limitepessoas);
+        }
         model.addAttribute("Link", links);
+        model.addAttribute("Reu",Reu);
+        model.addAttribute("Autor", Autor);
         model.addAttribute("urlThumbnail", companiesSevice.findAll().get(0).getUrlThumbnail() );
         model.addAttribute("localizador", UUID.randomUUID().toString());
         model.addAttribute("listprocessos", autorReuDTOList);
--- src/main/java/com/envio/email/services/AutorService.java
+++ src/main/java/com/envio/email/services/AutorService.java
@@ -1,28 +1,25 @@
 package com.envio.email.services;
 
+import com.envio.email.models.Cliente;
 import com.envio.email.models.Processo;
 import com.envio.email.models.Processo_autor;
-import com.envio.email.models.Processo_reu;
 import com.envio.email.repositories.AutorRepository;
-import com.envio.email.repositories.ReuRepository;
+import com.envio.email.repositories.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class ReuService {
+public class AutorService {
 
     @Autowired
-    private ReuRepository reuRepository;
+    private AutorRepository autorRepository;
 
     @Autowired
     AutorReuService autorReuService;
 
-    public List<Processo_reu> BuscarporId (Processo processo)  {
-        return reuRepository.findByID(processo);
+    public List<Processo_autor> BuscarporId (Processo processo)  {
+        return autorRepository.findByID(processo);
 
     }
-
 
 }
